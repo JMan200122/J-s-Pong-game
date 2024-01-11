c64bb9130af62a8e8e8d697fef1cd36bef384b67
JMan200122 <154390103+JMan200122@users.noreply.github.com>
2024-01-11 15:48:41 -0600

    Update pong-audio.js

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -60,7 +60,7 @@ ambientSound.player.volume.value = -20; //turn down volume
 export var adventureMusic = new soundFile("8-bit-space.mp3");
 soundArray.push(adventureMusic);
 adventureMusic.player.loop = true;
-adventureMusic.player.volume.value = 20;
+adventureMusic.player.volume.value = 40;
 
 export var villageMusic = new soundFile("silence.mp3");
 soundArray.push(villageMusic);
